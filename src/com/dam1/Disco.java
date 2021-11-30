be19a923b1b0993dff7bd3282393287b4ad640bb
Samuel Farto Vaamonde <sfartovaamonde@danielcastelao.org>
2021-11-30 10:55:35 +0100

    Clases y subclases

--- src/com/dam1/Disco.java
+++ src/com/dam1/Disco.java
@@ -1,4 +1,22 @@
 package com.dam1;
 
-public class Disco {
+public class Disco extends Multimedia {
+private String xenero;
+public Disco(){
+
+}
+
+    public Disco(String titulo, String autor, String formato, int duracion, String xenero) {
+        super(titulo, autor, formato, duracion);
+        this.xenero = xenero;
+    }
+
+    public void setXenero(String xenero) {
+        this.xenero = xenero;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
